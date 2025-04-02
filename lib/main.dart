47c2c091644062cf898660e3646507560a928a97
DevCat-HGS <prototiphs0108@gmail.com>
2025-04-01 20:32:14 -0500

    Dev

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SENA - Gestión de Asistencia',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
